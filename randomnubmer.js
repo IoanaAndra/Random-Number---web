a29547cafcba565b59a56958386a9f4515d57170
IoanaAndra <ioanaandrapopovici@gmail.com>
2020-05-15 01:35:36 +0100

    add Math.random

--- randomnubmer.js
+++ randomnubmer.js
@@ -1,5 +1,18 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML =Math.floor(-2.2);
+/*function myFunction() {
+   
+   document.getElementById("demo").innerHTML = Math.floor(-2.2);
+
+   console.log(NumberFloor);
 }
 
 myFunction();
+*/
+function RandomNumber() {
+
+    //document.getElementById("demo2").innerHTML =Math.random();
+  
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+}
+
+RandomNumber();
